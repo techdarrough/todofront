cb350b85407375a281f788920d2597b1809190dd
Preston Darrough <techdarrough@gmail.com>
2022-05-19 17:52:39 -0700

    Added useState and useEffect map fn for array map

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import Def from "../components/Def";
 
 
 
 
-const Main= (props) =>  {
-  let { todos } = props;
-let todoList = todos.map((todos, i) => <ListGroup.Item action variant="success">{todos}</ListGroup.Item>)
+const Main = (props) => {
+
+  const [todo, setTodo] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:3001/todos")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("DATA", data);
+        setTodo(data);
+      })
+      .catch((err) => {
+        resizeBy.status(500).json(err);
+        console.log(err)
+      });
+  }, []);
+
+  const handleTodo = (data) => {
+    setTodo(data)
+  }
+
+ 
+// map array into a table
+  let todoList = todo.map(({ todo_id, todo_name, description }) => (
+    <ListGroup.Item key={todo_id} >{todo_name}:{description}</ListGroup.Item>
+  ))
 
   return (
 
-  
-   <Def> 
-   <div>
-      <h1>My To Do List </h1>
 
-      <ListGroup>
-        { todoList }
-      </ListGroup>
-    </div>
+    <Def>
+      <div>
+        <h1>My To Do List </h1>
+
+        <ListGroup >
+          {todoList}
+        </ListGroup>
+      </div>
     </Def>
   );
 }
--- src/components/LowerMenu.js
+++ src/components/LowerMenu.js
@@ -2,17 +2,24 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Def from "./Def";
 
+
+
 export default function LowerMenu() {
   return (
-    <div>
-      <div className="d-grid gap-2">
-        <Button variant="primary" size="lg">
-          Edit
-        </Button>
-        <Button variant="secondary" size="lg">
-          Delete
-        </Button>
+    <Def>
+      <div>
+        <div className="d-grid gap-2">
+          <Button variant="primary" size="lg">
+            Edit
+          </Button>
+          <Button variant="success" size="lg">
+            create
+          </Button>
+          <Button variant="danger" size="lg">
+            Delete
+          </Button>
+        </div>
       </div>
-    </div>
+    </Def>
   );
 }
